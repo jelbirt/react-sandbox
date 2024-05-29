32c45c39b5babccda866cb729415ec13be0762a9
Jelbirt <jelbirt@worcester.edu>
2024-05-29 15:20:32 -0400

    reinitialized

---
+++ react-app1/src/Food.jsx
@@ -0,0 +1,15 @@
+function Food() {
+
+    const food1 = "Apple"
+    const food2 = "Pear"
+
+    return(
+        <ul>
+            <li>Banana</li>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    );
+}
+
+export default Food
---
+++ react-app1/src/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+
+    return(
+        <header>
+            <h1>
+                My website
+            </h1>
+            <nav>
+                <ul>
+                    <li><a href="URLHERE">Home</a></li>
+                    <li><a href="URLHERE">About</a></li>
+                    <li><a href="https://www.google.com/">Services</a></li>
+                    <li><a href="URLHERE">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header
