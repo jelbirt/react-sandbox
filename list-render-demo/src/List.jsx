3c944459a1eede6f99a2da3ec5b04c8c36bfb43e
Jacob Elbirt <jelbirt@worcester.edu>
2024-05-31 11:20:33 +0000

    finished list - includes array and mixed type props

--- list-render-demo/src/List.jsx
+++ list-render-demo/src/List.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 
 function List(props) {
 
@@ -26,6 +27,15 @@ function List(props) {
             <ol className="list-items">{listItems}</ol>
         </>
     );
+}
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number, name: PropTypes.string, calories: PropTypes.number})),
+}
+List.defaultProps = {
+    category: "Category",
+    items: [],
+
 }
 
 export default List
