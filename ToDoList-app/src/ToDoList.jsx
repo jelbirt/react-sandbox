008651d54a76c1dc8b1922e5922fc5a4d505bb97
Jacob Elbirt <jelbirt@worcester.edu>
2024-06-11 19:56:10 +0000

    completed to do list app

--- ToDoList-app/src/ToDoList.jsx
+++ ToDoList-app/src/ToDoList.jsx
@@ -10,19 +10,31 @@ function ToDoList() {
     }
 
     function addTask() {
-
+        if (newTask.trim() !== "") {
+            setTasks(t => [...t, newTask]);
+            setNewTask("");
+        }
     }
 
     function deleteTask(index) {
-
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
     }
 
     function moveTaskUp(index) {
-
+        if (index > 0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index-1], updatedTasks[index]];  // swaps two elements in an array
+            setTasks(updatedTasks);
+        }
     }
 
     function moveTaskDown(index) {
-
+        if (index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];  // swaps two elements in an array
+            setTasks(updatedTasks);
+        }
     }
 
     return(
