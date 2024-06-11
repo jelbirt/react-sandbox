5b7df42c52d6b39d39f949b6cc789f20b9fda82b
Jacob Elbirt <jelbirt@worcester.edu>
2024-06-11 13:17:37 +0000

    started todo list - need update somewhere

---
+++ ToDoList-app/src/ToDoList.jsx
@@ -0,0 +1,76 @@
+import React, {useState} from 'react';
+
+function ToDoList() {
+
+    const [tasks, setTasks] = useState(["Do Homework", "Sweep the Floors", "Wash the Dishes"]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event) {
+        setNewTask(event.target.value);
+    }
+
+    function addTask() {
+
+    }
+
+    function deleteTask(index) {
+
+    }
+
+    function moveTaskUp(index) {
+
+    }
+
+    function moveTaskDown(index) {
+
+    }
+
+    return(
+    <div className="to-do-list">
+        <h1>To-Do-List</h1>
+
+        <div>
+            <input 
+                type="text" 
+                placeholder="Enter a task..."
+                value={newTask}
+                onChange={handleInputChange}/>
+
+            <button
+                className="add-button"
+                onClick={addTask}>
+                
+            Add</button>
+        </div>
+
+        <ol>
+            {tasks.map((task, index) => 
+                <li key={index}>
+                    <span className="text">{task}</span>
+                    <button
+                        className="delete-button"
+                        onClick={() => deleteTask(index)}>
+                        Delete
+                    </button>
+
+                    <button
+                        className="move-button"
+                        onClick={() => moveTaskUp(index)}>
+                        Move UP
+                    </button>
+
+                    <button
+                        className="move-button"
+                        onClick={() => moveTaskDown(index)}>
+                        Move DOWN
+                    </button>
+                </li>)}
+        </ol>
+
+
+
+    </div>
+    );
+}
+
+export default ToDoList
